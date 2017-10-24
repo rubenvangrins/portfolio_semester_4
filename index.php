0315d9e64227e265fa474da892753192b997b238
Unknown <Rubenvangrinsven@hotmail.com>
2017-10-24 10:30:09 +0200

    Portfolio v1
    Dit is de eerste versie van mijn nieuwe portfolio.
    In deze versie is alleen de homepage nog beschikbaar.

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="res/css/style.css" media="all" />
+	<!-- FONT -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+	<!-- JS -->
+	<script language="javascript" type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+</head>
+<body>
+	
+	<header>
+		<nav>
+			<a class="glitchy logo" href="#!" data-text="Ruben van Grinsven">Ruben van Grinsven</a>	
+			
+			<ul>
+				<li><a class="glitchy" href="#portfolio" data-text="Portfolio">Portfolio</a></li>
+				<li><a class="glitchy" href="#about" data-text="About me">About me</a></li>
+				<li><a class="glitchy" href="#contact" data-text="Contact">Contact</a></li>
+			</ul>
+			
+		</nav>
+		
+			<span class="side_label_left"><p class="glitchy">DESIGNER</p></span>
+			<span class="side_label_right"><p class="glitchy">PROGRAMMER</p></span>
+			
+			<h2 class="type_title">
+				<span class="scramble glitchy" data-text="I work with">I work with</span>
+				<div class="typewrite ik-ben" data-period="2000" data-type='[ "Photoshop", "Illustrator", "HTML + CSS", "PHP", "Javascript" ]'>
+					<span class="wrap"></span>
+				</div>
+			</h2>
+
+		<canvas id="tv"></canvas>
+	</header>
+	
+	<script src="res/js/static.js"></script>
+	<script src="res/js/glitch.js"></script>	
+	<script src="res/js/type.js"></script>
+</body>
+</html>
